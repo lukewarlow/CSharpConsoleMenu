8dadc394349b82694d3f97cb02bfa91a36d0fa6a
Luke Warlow <lukewarlow156@gmail.com>
2018-08-11 19:43:09 +0100

    Initial commit

---
+++ CSharpConsoleMenu/AbstractMenu.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpConsoleMenu
+{
+    public abstract class AbstractMenu
+    {
+        private string Title { get; }
+
+        private readonly List<MenuItem> MenuItems;
+
+        protected AbstractMenu(string title)
+        {
+            Title = title;
+            MenuItems = new List<MenuItem>();
+            Init();
+        }
+
+        protected abstract void Init();
+        
+        protected virtual void UpdateMenuItems() {}
+
+        public void Display()
+        {
+            var repeat = true;
+            while (repeat)
+            {
+                UpdateMenuItems();
+                Console.WriteLine();
+                Console.WriteLine(Title);
+                for (var i = 0; i < MenuItems.Count; i++)
+                {
+                    if (MenuItems[i].IsVisible)
+                        Console.WriteLine(i + ". " + MenuItems[i].Description);
+                }
+
+                Console.Write("Select Option: ");
+                var input = Console.ReadLine();
+
+                try
+                {
+                    var itemIndex = int.Parse(input);
+                    var menuItem = MenuItems[itemIndex];
+                    if (menuItem.IsVisible) repeat = menuItem.Run();
+                    else throw new InvalidOperationException();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid option, you need to enter a number.");
+                    repeat = true;
+                    
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine($"Invalid option. Option {input} doesn't exist.");
+                    repeat = true;
+                }
+                
+                catch (InvalidOperationException)
+                {
+                    Console.WriteLine($"Invalid Operation. Option at {input} is hidden.");
+                    repeat = true;
+                }
+            }
+        }
+
+        public void AddMenuItem(MenuItem menuItem)
+        {
+            if (!MenuItems.Contains(menuItem)) MenuItems.Add(menuItem);
+            else throw new InvalidOperationException();
+        }
+        
+        public void AddHiddenMenuItem(MenuItem menuItem)
+        {
+            AddMenuItem(menuItem.Hide());
+        }
+
+        public void ShowMenuItem(long itemId)
+        {
+            try
+            {
+                var menuItem = new MenuItem(itemId);
+                var index = MenuItems.IndexOf(menuItem);
+                MenuItems[index].Show();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Error showing menu item: "  + e);
+            }
+        }
+        
+        public void HideMenuItem(long itemId)
+        {
+            try
+            {
+                var menuItem = new MenuItem(itemId);
+                var index = MenuItems.IndexOf(menuItem);
+                MenuItems[index].Hide();
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Error showing menu item: "  + e);
+            }
+        }
+    }
+}
---
+++ LibraryTest/TestMenu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using CSharpConsoleMenu;
+
+namespace LibraryTest
+{
+    public class TestMenu : AbstractMenu
+    {
+        private bool _showHiddenMenu;
+        
+        public TestMenu() : base("Welcome to the test menu.") {}
+
+        protected override void Init()
+        {
+            AddMenuItem(new MenuItem(0, "Exit menu").SetAsExitOption());
+            AddMenuItem(new MenuItem(1, "Test sub menu", new TestSubMenu()));
+            
+            AddMenuItem(new MenuItem(2, "Show hidden menu item", () =>
+            {
+                Console.WriteLine("Showing hidden menu item");
+                _showHiddenMenu = true;
+            }));
+            
+            AddHiddenMenuItem(new MenuItem(3, "Hidden menu item", () => Console.WriteLine("I was a hidden menu item")));
+        }
+
+        protected override void UpdateMenuItems()
+        {
+            if (_showHiddenMenu) ShowMenuItem(3);
+        }
+    }
+    
+    public class TestSubMenu : AbstractMenu
+    {
+        public TestSubMenu() : base("Welcome to the test sub menu.") {}
+
+        protected override void Init()
+        {
+            AddMenuItem(new MenuItem(0, "Exit current menu").SetAsExitOption());
+            AddMenuItem(new MenuItem(1, "Test submenu item", () => Console.WriteLine("Test sub menu item selected")));
+        }
+    }
+}
